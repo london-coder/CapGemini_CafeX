4a09a1c56954c9a79d03e1afaeda33c965ff12d3
Alan Lewis <london-coder@users.noreply.github.com>
2019-05-11 13:39:07 +0100

    Remove a duplicate test, and code clean up. (#2)

--- src/test/scala/example/CafexStep1Spec.scala
+++ src/test/scala/example/CafexStep1Spec.scala
@@ -3,15 +3,16 @@ package example
 import org.scalatest._
 
 class CafexStep1Spec extends FlatSpec with Matchers {
+  import Cafex._
   /**
    * These are the tests for STEP 1 
    */
   "The Cafe object" should "contain the menu" in {
-    Cafex.menuSize shouldEqual 4
+    menuSize shouldEqual 4
   }
 
   "The cost of cola coffee and cheese sandwich" should "sum to 3.5" in {
-    Cafex.cost(List("Cola", "Coffee", "Cheese Sandwich")) shouldEqual 3.5
+    cost(List("Cola", "Coffee", "Cheese Sandwich")) shouldEqual 3.5
   }
 
 }
